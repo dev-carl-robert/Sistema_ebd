ebbafc12d9256b6761819da0eced8881a491efca
dev-carl-robert <dev.carl.robert@gmail.com>
2024-06-13 10:15:33 -0300

    implementando função de cadastrar aluno em uma classe

--- script_1.py
+++ script_1.py
@@ -1,18 +1,21 @@
-Alunos = [
-    {"nome": "Miguel", "idade": 17, "sexo": "masculino", "telefone": "9####-####"},
-    {"nome": "Apolo", "idade": 17, "sexo": "masculino", "telefone": "9####-####"},
-    {"nome": "Silas", "idade": 15, "sexo": "masculino", "telefone": "9####-####"}
-]
-def exibir_alunos():
-    print("tabela atual")
-    for aluno in Alunos:
-        for chave, valor in aluno.items():
-            print(f"{chave} : {valor}")
-        print() 
+continuar = "s"
 
+Alunos = [
+    {"nome": "Miguel", "sobrenome": "Santos", "idade": "17", "sexo": "masculino", "telefone": "9####-####", "classe": "jovens"},
+    {"nome": "Robert", "sobrenome": "Santos", "idade": "22", "sexo": "masculino", "telefone": "9####-####", "classe": "jovens"},
+]
+
+def exibir_alunos():
+    print("Alunos cadastrados:")
+    indice = 0
+    for indice, aluno in enumerate(Alunos, start=1):
+            print(f"{indice} - {aluno['nome']} {aluno['sobrenome']}")
+    print()
+    
 def cadastrar_Aluno():
     while True:
         cadastrar_nome_aluno = str(input("Qual Aluno quer cadastrar? ")).strip()  
+        cadastrar_sobrenome_aluno = str(input("Qual o sobrenome? ")).strip()  
         if cadastrar_nome_aluno:
             break
         print("nome do aluno não pode ficar em branco")
@@ -28,19 +31,29 @@ def cadastrar_Aluno():
             
     cadastrar_sexo_aluno = str(input("Qual a sexo deste aluno? "))    
     cadastrar_telefone_aluno = str(input("Qual o telefone deste aluno? "))
+    cadastrar_classe_aluno = str(input("Qual a classe deste aluno? "))
     
     novo_aluno = {
-        "nome": cadastrar_nome_aluno,
+        "nome": cadastrar_nome_aluno.capitalize(),
+        "sobrenome": cadastrar_sobrenome_aluno.capitalize(),
         "idade": cadastrar_idade_aluno,
         "sexo": cadastrar_sexo_aluno,
         "telefone": cadastrar_telefone_aluno,
+        "classe": cadastrar_classe_aluno
     }
     
     Alunos.append(novo_aluno)
     print(f"Aluno: {cadastrar_nome_aluno} cadastrado com sucesso")
+    print()
 
-exibir_alunos() #exibir antes do cadastro
-cadastrar_Aluno() #cadastrar alunos
-exibir_alunos() #exibir depois do cadastro
 
+
+
+while continuar == "s":
+    exibir_alunos() # mostrar todos os alunos
+    cadastrar_Aluno() #cadastrar alunos
+    exibir_alunos() #exibir depois do cadastro
+    continuar = input("Deseja cadastrar mais alguem? responda 's' para sim, e 'n' para não: ")
+else:
+    print("Volte sempre")
     
