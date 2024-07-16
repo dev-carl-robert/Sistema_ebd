33584cd257f2888a08e260ad45a2bae20ca50061
Carlos Robert <149803558+dev-carl-robert@users.noreply.github.com>
2024-07-15 21:10:33 -0300

    função acessar

--- auxiliar_testes.py
+++ auxiliar_testes.py
@@ -4,34 +4,22 @@ import pandas as pd
 # import re
 
 ROOT_PATH = Path(__file__).parent
-caminho_csv = ROOT_PATH / "alunos.csv"
+alunos_caminho = ROOT_PATH / "alunos.csv"
 
 
-nome = input("Qual o nome do aluno? ")
-sobrenome = input("Qual o sobrenome do aluno? ")
-data_de_nascimento = input("Qual a data de nascimento do aluno? ex: 01.01.2002 : ")
-datas_formatadas = pd.to_datetime(data_de_nascimento, format='%d.%m.%Y')
-idade = 2024 - datas_formatadas.year
-sexo = input("Qual o sexo do aluno? ")
-telefone = str(input("Qual o telefone do aluno? ex: 987112233 : "))
-classe = input("Qual a classe do aluno? ")
-
-if caminho_csv.exists():
-    Alunos_df = pd.read_csv(caminho_csv)
-    ultima_matricula = int(Alunos_df['matricula'].iloc[-1])
-    indice = ultima_matricula + 1
-
+def cadastrar_aluno():
+    nome = input("Digite o nome do aluno: ").capitalize()
     
-with open(caminho_csv, "a", encoding="utf-8", newline="") as arquivoAluno:
-    escritorAluno = csv.writer(arquivoAluno)
-    escritorAluno.writerow([indice, nome, sobrenome, idade, (data_de_nascimento.replace(".", "/")), sexo, telefone, classe])
+    Alunos_df = pd.read_csv(alunos_caminho)
+    if Alunos_df['matricula'].dropna().size > 0:
+        indice = 1        
+    else:
+        ultima_matricula = int(Alunos_df['matricula'].iloc[-1])
+        indice = ultima_matricula + 1
 
-print(Alunos_df)
+    with open(alunos_caminho, "a", encoding="utf-8", newline="") as arquivoAluno:
+        escritorAluno = csv.writer(arquivoAluno)
+        escritorAluno.writerow([indice, nome])
+        print(f"Aluno {nome} cadastrado com sucesso")
 
-# excluir
-# Alunos_df = Alunos_df[Alunos_df['nome'] != 'mariana']
-# Alunos_df.to_csv(caminho_csv, index=False, encoding='utf-8')
-
-
-# for row in Alunos_df:
-#     print(row)
+cadastrar_aluno()
--- backend/script_2.py
+++ backend/script_2.py
@@ -13,15 +13,15 @@ professores_df = pd.read_csv(professores_caminho)
 print()
 print("Bem vindo ao Sistema de gestão da Escola Biblica Dominical")
 
-user = input("Quem está acessando? ")
-print()
+# user = input("Quem está acessando? ")
+# print()
 
-password = input("Digite sua senha: " )
-print()
+# password = input("Digite sua senha: " )
+# print()
 
-print()
-print(f"Seja Bem Vindo {user.capitalize()} como posso ajudá-lo?")
-print()
+# print()
+# print(f"Seja Bem Vindo {user.capitalize()} como posso ajudá-lo?")
+# print()
 
 def menu():
      print("""
@@ -45,39 +45,55 @@ def exibir_professores():
     print(professores_df)
 
 def cadastrar_aluno():
-    nome = input("Digite o nome do aluno: ")
-    sobrenome = input("Digite o sobrenome do aluno: " )
-    data_de_nascimento = input("Digite a data de nascimento do aluno: # ex. 01.01.2002 : ")
-    datas_formatadas = pd.to_datetime(data_de_nascimento, format='%d.%m.%Y')
-    idade = 2024 - datas_formatadas.year
-    sexo = input("digite o sexo do aluno. F para feminino e M para masculino: ")
-    telefone = str(input("digite o telefone do aluno: ex. 987871123 : "))
     
     if alunos_caminho.exists():
         Alunos_df = pd.read_csv(alunos_caminho)
         ultima_matricula = int(Alunos_df['matricula'].iloc[-1])
         indice = ultima_matricula + 1
     
-    classe = input("digite a classe do aluno: ")
+    nome = input("Digite o nome do aluno: ").capitalize()
+    print(nome)
+    sobrenome = input("Digite o sobrenome do aluno: " ).capitalize()
+    print(sobrenome)
+    for index, row in Alunos_df.iterrows():
+        if nome.strip().lower() == row['nome'].strip().lower() and sobrenome.strip().lower() == row['sobrenome'].strip().lower():
+            print(f"{nome} {sobrenome} já foi cadastrado")
+            nome = input("Digite o nome do aluno: ").capitalize()
+            sobrenome = input("Digite o sobrenome do aluno: " ).capitalize()
+    else:
+        data_de_nascimento = input("Digite a data de nascimento do aluno: # ex. 01.01.2002 : ")
+        datas_formatadas = pd.to_datetime(data_de_nascimento, format='%d.%m.%Y')
+        idade = 2024 - datas_formatadas.year
+        sexo = input("digite o sexo do aluno. F para feminino e M para masculino: ").capitalize()
+        telefone = str(input("digite o telefone do aluno: ex. 987871123 : "))
+        classe = input("digite a classe do aluno: ").capitalize()
+    
     with open(alunos_caminho, "a", encoding="utf-8", newline="") as arquivoAluno:
         escritorAluno = csv.writer(arquivoAluno)
         escritorAluno.writerow([indice, nome, sobrenome, idade, (data_de_nascimento.replace(".", "/")), sexo, telefone, classe])
         print(f"Aluno {nome} {sobrenome} cadastrado com sucesso")
 
 def cadastrar_professor():
-    nome = input("digite o nome do professor: ")
-    sobrenome = input("digite o sobrenome do professor: ")
-    data_de_nascimento = input("digite a data de nascimento do professor: ex. 01.01.2002 : ")
-    data_formatada = pd.to_datetime(data_de_nascimento, format='%d.%m.%Y')
-    idade = 2024 - data_formatada.year
-    sexo = input("digite o sexo do professor. F para feminino e M para masculino : ")
-    telefone = str(input("digite o telefone do professor. ex. 987371023 : "))
-    classe = input("digite a classe do professor: ")
     
     if professores_caminho.exists():
         professores_df = pd.read_csv(professores_caminho)
         ultima_matricula = int(professores_df['matricula'].iloc[-1])
         indice = ultima_matricula + 1
+    
+    nome = input("digite o nome do professor: ").capitalize()
+    sobrenome = input("digite o sobrenome do professor: ").capitalize()
+    for index,row in professores_df.iterrows():
+        if nome.strip().lower() == row["nome"].strip().lower() and sobrenome.strip().lower() == row["sobrenome"].strip().lower():
+            print(f"{nome} {sobrenome} já cadastrado")
+            nome = input("digite o nome do professor: ").capitalize()
+            sobrenome = input("digite o sobrenome do professor: ").capitalize()
+    else:
+        data_de_nascimento = input("digite a data de nascimento do professor: ex. 01.01.2002 : ")
+        data_formatada = pd.to_datetime(data_de_nascimento, format='%d.%m.%Y')
+        idade = 2024 - data_formatada.year
+        sexo = input("digite o sexo do professor. F para feminino e M para masculino : ").capitalize()
+        telefone = str(input("digite o telefone do professor. ex. 987371023 : "))
+        classe = input("digite a classe do professor: ").capitalize()
     
     with open(professores_caminho, "a", encoding="utf-8", newline="") as arquivoAluno:
         escritorProfessor = csv.writer(arquivoAluno)
@@ -85,6 +101,68 @@ def cadastrar_professor():
         
         print(f"Professor {nome} {sobrenome} cadastrado com sucesso!!")
         
+def acessar_aluno():
+    Alunos_df = pd.read_csv(alunos_caminho)
+    for index, row in Alunos_df.iterrows():
+        print(f"{row['matricula']} - {row['nome']} {row['sobrenome']}")
+    
+    while True:
+        nome_aluno_escolhido = input("Qual o nome do aluno deseja acessar? ")
+        sobrenome_aluno_escolhido = input("Qual o sobrenome do aluno deseja acessar? ")
+        
+        # Procurar o aluno no dataframe
+        aluno_encontrado = False
+        for index, row in Alunos_df.iterrows():
+            if (nome_aluno_escolhido.strip().lower() == row['nome'].strip().lower() and
+                sobrenome_aluno_escolhido.strip().lower() == row['sobrenome'].strip().lower()):
+                print()
+                print(f"➖ Matricula: {row['matricula']}")
+                print(f"➖ Nome: {row['nome']}")
+                print(f"➖ Sobrenome: {row['sobrenome']}")
+                print(f"➖ Idade: {row['idade']}")
+                print(f"➖ Data de Nascimento: {row['data_de_nascimento']}")
+                print(f"➖ Telefone: {row['telefone']}")
+                print(f"➖ Sexo: {row['sexo']}")
+                print(f"➖ Classe: {row['classe']}")
+                aluno_encontrado = True
+                break
+        
+        if aluno_encontrado:
+            break
+        else:
+            print("Este aluno não está cadastrado, informe outro aluno.")
+
+       
+def acessar_professor():
+    professores_df = pd.read_csv(professores_caminho)
+    for index, row in professores_df.iterrows():
+        print(f"{row['matricula']} - {row['nome']} {row['sobrenome']}")
+    
+    while True:
+        nome_professor_escolhido = input("Qual o nome do aluno desejado? ")
+        sobrenome_professor_escolhido = input("Qual o sobrenome do aluno desejado? ")
+
+        aluno_encontrado = False
+        for index, row in professores_df.iterrows():
+            if nome_professor_escolhido.strip().lower() == row['nome'].strip().lower() and sobrenome_professor_escolhido.strip().lower() == row['sobrenome'].strip().lower():
+                print()
+                print(f"➖ Matricula: {row['matricula']}")
+                print(f"➖ Nome: {row['nome']}")
+                print(f"➖ Sobrenome: {row['sobrenome']}")
+                print(f"➖ Idade: {row['idade']}")
+                print(f"➖ Data de Nascimento: {row['data_de_nascimento']}")
+                print(f"➖ Telefone: {row['telefone']}")
+                print(f"➖ Sexo: {row['sexo']}")
+                print(f"➖ Classe: {row['classe']}")
+                aluno_encontrado = True
+                break
+        
+        if aluno_encontrado:
+            break
+        else: 
+            print("Este aluno não está cadastrado, informe outro aluno.")
+
+
 # Chamada de funções
 while True:
     print()
@@ -123,7 +201,7 @@ while True:
         case "7":
             print("Aguarde um instante...........")
             print()
-            lista_de_domingos_do_trimestre()
+            # lista_de_domingos_do_trimestre()
         
         case "0":
             print("Volte sempre")
